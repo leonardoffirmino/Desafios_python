107f744d01363cb24a12d17a38b240c0a78e1532
leonardoffirmino <firmino750@gmail.com>
2024-07-31 22:21:10 -0300

    add funcao marcar favorito

--- main.py
+++ main.py
@@ -10,9 +10,30 @@ def add_contact(list_contacts,name,email,phone):
 def visualize_list(list_contacts):
   print("\n Contatos da agenda:")
   for indice,item in enumerate(list_contacts,start=1):
-    status = "✓" if item["Favorito"] else " " 
+    status = "✓" if item["Favorito"] else "Não favorito" 
 
-    print(f"{indice}. [{status} {item['Name']}]")
+    print(f"{indice}. [{item['Name']} - {status} ]")
+  return
+
+def delete_contact(list_contacts):
+  visualize_list(list_contacts)
+  index_delete = int(input("Informe o indice  que deseja apagar!"))
+  if 0 <= index_delete < len(list_contacts):
+    del list_contacts[index_delete]
+    print("Contato deletado de agenda!")
+  else:
+    print("Indice inválido!")
+  return 
+
+
+def add_contact_favorite(list_contacts):
+  visualize_list(list_contacts)
+
+  item_favorite = int(input("Favor informar qual indice deseja favoritar ou desmarcar em agenda!"))
+
+  if 0 <= item_favorite < len(list_contacts):
+    list_contacts[item_favorite]['Favorito'] = not list_contacts[item_favorite]['Favorito']
+    print("Contato marcado ou desmarcado como favorito!")
   return
 
 
@@ -25,11 +46,11 @@ while True:
   print("1. Criação de cadastro em agenda.")
   print("2. Visualisar itens de agenda.")
   print("3. Deletar cadastro da agenda.")
-  print("4. Marcar como concluído item da agenda.")
+  print("4. Marcar como Favorito item da agenda.")
   print("5. Editar cadastro da agenda.")
   print("7. Sair")
 
-  input_choice = input("Digite sua esolha:")
+  input_choice = input("Digite sua escolha:")
 
 
   if input_choice == "1":
@@ -39,3 +60,7 @@ while True:
     add_contact(list_contacts,name,email,phone)
   elif input_choice =="2":
     visualize_list(list_contacts)
+  elif input_choice =="3":
+    delete_contact(list_contacts)
+  elif input_choice =="4":
+    add_contact_favorite(list_contacts)
